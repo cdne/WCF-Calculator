9c276e52e7a1a0534477c687646e984106caf9e3
Claudiu <cduneata@gmail.com>
2020-08-19 12:42:42 +0300

    Add calculator class

---
+++ DerivativesCalculatorService/App_Code/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerivativesCalculatorService.App_Code
+{
+    internal class Calculator : IDerivativesCalculator
+    {
+        #region IDerivativesCalculator Members
+        public decimal CalculateDerivative(int days, string[] symbols, string[] functions)
+        {
+            return DateTime.Now.Millisecond;
+        }
+        #endregion
+    }
+}
